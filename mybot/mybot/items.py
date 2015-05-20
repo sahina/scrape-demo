0fc5d8499f4d4b6a64800ec038d3e2b13df13fea
Altug Sahin <altug.sahin@credit-suisse.com>
2015-05-20 13:51:53 -0400

    added country scrape

--- mybot/mybot/items.py
+++ mybot/mybot/items.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
-import scrapy
+from scrapy import Field, Item
 
 
-class MybotItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class CountryItem(Item):
+    name = Field()
+    url = Field()
+
+class TeamItem(Item):
+    name = Field()
--- mybot/mybot/spiders/server.py
+++ mybot/mybot/spiders/server.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors import LinkExtractor
+from mybot.items import CountryItem, TeamItem
 
 
 class ServerSpider(CrawlSpider):
@@ -11,9 +12,26 @@ class ServerSpider(CrawlSpider):
     ]
 
     rules = [
-        Rule(LinkExtractor(allow='.*'), follow=True, callback='parse_links')
+        # Rule(LinkExtractor(allow='.*'), callback='parse_links', follow=True),
+        Rule(LinkExtractor(allow='countries\.html'), callback='parse_countries', follow=True),
+        Rule(LinkExtractor(allow='c-.*\.html'), callback='parse_country', follow=True)
     ]
 
-    def parse_links(self, response):
-        print '>>> parse_links'
+    def parse_country(self, response):
+        print 'parse_country >>> {0} ***************'.format(response.url)
+        for c in response.xpath("//li"):
+            item = TeamItem()
+            item["name"] = c.xpath("./text()").extract()
 
+            yield item
+
+
+    def parse_countries(self, response):
+        print 'parse_countries >>>  {0} ***************'.format(response.url)
+        for c in response.xpath("//a[@class='country-link']"):
+            item = CountryItem()
+            item["name"] = c.xpath('./text()').extract()
+            item["url"] = c.xpath('./@href').extract()
+
+            yield item
+        
